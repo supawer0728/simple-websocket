00e3937fbc5195362f5b0fc54c9acb4f9b88dffa
supawer0728 <supawer0728@gmail.com>
2018-03-30 00:26:55 +0900

    WebSocket 리팩토링

--- src/main/java/com/parfait/study/simplewebsocket/SimpleWebsocketApplication.java
+++ src/main/java/com/parfait/study/simplewebsocket/SimpleWebsocketApplication.java
@@ -2,11 +2,21 @@ package com.parfait.study.simplewebsocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SimpleWebsocketApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleWebsocketApplication.class, args);
+	}
+
+	@Controller
+	public static class MainController {
+		@GetMapping("/")
+		public String main() {
+			return "redirect:/chat/rooms";
+		}
 	}
 }
